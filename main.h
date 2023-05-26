65c361d6c1078c30b254674c7d9ec59785e57b7e
Philip Zatta, PhD <71414377+pzzatta@users.noreply.github.com>
2023-05-25 21:32:11 -0600

    Create main.h

---
+++ main.h
@@ -0,0 +1,54 @@
+/*
+ ******************************************************************************
+ * File              : main.h
+ * Author            : Philip Zatta, PhD, Software Consultant
+ * Company           : Embedded Software & Systems LLC
+ * MCU/Board         : Waveshare OpenH743 STM32H743IIT6 Development Board
+ * Description       : System clock setup to max frequency of 480 MHz
+ * Datasheet         : DS12110 Rev March 2023  STM32H742xI/G STM32H743xI/G
+ * Reference Manual  : RM0433 Reference Manual Rev 8, January 2023
+ *
+ * Key Features      : 32-bit Arm® Cortex®-M7 core with double precision
+ *                     FPU and L1 cache: 16 Kbytes of data
+ *                     and 16 Kbytes of instruction cache; frequency
+ *                     up to 480 MHz, MPU, 1027 DMIPS/
+ *                     2.14 DMIPS/MHz (Dhrystone 2.1), and DSP instructions
+ *
+ * Author            : Philip Zatta, PhD, Software Consultant
+ * Company           : Emebedded Software & Systems LLC
+ *                     Embedded Software & LabView
+ * IDE               : STM32CUBE Version 1.12
+ * Windows OS        : WIndows 11
+ * Date              : May 23, 2023
+ ******************************************************************************/
+
+/* ------------------ Define to prevent recursive inclusion ------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+#include <stdint.h>
+#include "mco_pins_config.h"
+#include "mco_select_set.h"
+#include "system_clock_config.h"
+
+
+/************************ Function prototypes ***************************/
+extern void SystemInit(void);              // ST Microelectronics function
+extern void SystemCoreClockUpdate(void);   // ST Microelectronics function
+
+/************************** Global Variables ***************************/
+extern uint32_t SystemD1Clock;
+extern uint32_t SystemD2Clock;
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
